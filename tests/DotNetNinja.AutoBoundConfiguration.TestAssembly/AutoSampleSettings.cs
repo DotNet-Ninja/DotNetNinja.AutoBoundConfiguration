085f01c20eb09e471f1522445019a4d032f49b91
Larry House <dotnet-ninja@users.noreply.github.com>
2020-02-08 14:05:57 -0600

    Added Automatic section based on class name & more tests

---
+++ tests/DotNetNinja.AutoBoundConfiguration.TestAssembly/AutoSampleSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DotNetNinja.AutoBoundConfiguration.TestAssembly
+{
+    [AutoBind()]
+    [ExcludeFromCodeCoverage]
+    public class AutoSampleSettings
+    {
+        public string StringSetting { get; set; }
+
+        public int IntegerSetting { get; set; }
+
+        public static readonly SampleSettings Default = new SampleSettings { StringSetting = "A String", IntegerSetting = 1234 };
+    }
+}
---
+++ tests/DotNetNinja.AutoBoundConfiguration.Tests/TestConfigurationSource.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetNinja.AutoBoundConfiguration.Tests
+{
+    public static class TestConfigurationSource
+    {
+        public static IConfigurationRoot Default
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("Sample:StringSetting", "String"),
+                        new KeyValuePair<string, string>("Sample:IntegerSetting", "1234")
+                    });
+                return builder.Build();
+            }
+        }
+
+        public static IConfigurationRoot AutoSectionFinding
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("AutoSampleSettings:StringSetting", "AutoString"),
+                        new KeyValuePair<string, string>("AutoSampleSettings:IntegerSetting", "1234")
+                    });
+                return builder.Build();
+            }
+        }
+    }
+}
